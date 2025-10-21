8d52dc1f93b01766e7942385cb4ca1841ccde4f3
ElJ28Gamer <elj28fer@gmail.com>
2025-10-21 19:28:15 +0200

    Documented code

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #Imports
-from util1 import cam
+from util1 import cam           #Tool 1 code and functionality
 
 #Defs
-def menu():
+def menu():                     #Prints what tools have
     print("")
     print("//  Englobed Tools  //")
     print("")
     print(" - Press 1 to open the camera")
-    print(" - Press 2 to ")
-    print(" - Press 3 to ")
-    print(" - Press 4 to ")
+    print(" - Press 2 to ?")
+    print(" - Press 3 to ?")
+    print(" - Press 4 to ?")
     print(" - To exit, press Enter")
     print("")
     print("//  Englobed Tools  //")
@@ -17,19 +17,19 @@ def menu():
     
 #Functions
 menu()
-x = int(input())
+x = int(input())    #Waits until a number is written
 
-if x == 1:
+if x == 1:          #Opens tool 1
     cam()
     menu()
-elif x == 2:
+elif x == 2:        #Opens tool 1
     2()
     menu()
-elif x == 3:
+elif x == 3:        #Opens tool 1
     3()
     menu()
-elif x == 4:
+elif x == 4:        #Opens tool 1
     4()
     menu()
-else:
+else:               #Exits
     exit
