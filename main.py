bac68ed940bcafc1d051a5cc8a8c7a79064bf692
ElJ28Gamer <elj28fer@gmail.com>
2025-10-22 17:21:24 +0200

    test

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #Imports
-from util1 import cam           #Tool 1 code and functionality
+import cv2 as cv                 #Camera lib
+
+#Variables
+numberlist = [1, 2, 3, 4]
 
 #Defs
-def menu():                     #Prints what tools have
+def menu():                      #Prints what tools have
     print("")
     print("//  Englobed Tools  //")
     print("")
@@ -14,22 +17,44 @@ def menu():                     #Prints what tools have
     print("")
     print("//  Englobed Tools  //")
     print("")
-    
-#Functions
-menu()
-x = int(input())    #Waits until a number is written
 
-if x == 1:          #Opens tool 1
-    cam()
+def selection():
+    if x == 1:          #Opens tool 1
+     cam()
+    elif x == 2:        #Opens tool 2
+     cam()
+    elif x == 3:        #Opens tool 3
+     cam()
+    elif x == 4:        #Opens tool 4
+     cam()
+    else:               #Exits
+     exit
+    
+def cam():
+ cap = cv.VideoCapture(0)
+
+ if not cap.isOpened():     #Opens cam, if isn't opened
+    print("NO")
+    exit()
+
+ while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("exit")
+        break
+
+    color = cv.cvtColor(frame, cv.COLOR_BGR2BGRA)
+    cv.imshow("CUSTOM CAMERA", color)                               #Custom name
+    if cv.waitKey(1) == ord("q") or cv.waitKey(1) == ord("Q"):      #Keys to exit
+        break
+
+ cap.release()                  #Stops capture
+ cv.destroyAllWindows()         #Closes window
+
+while True:
     menu()
-elif x == 2:        #Opens tool 1
-    2()
-    menu()
-elif x == 3:        #Opens tool 1
-    3()
-    menu()
-elif x == 4:        #Opens tool 1
-    4()
-    menu()
-else:               #Exits
-    exit
+    x = int(input())    #Waits until a number is written
+    selection()
+    if x != numberlist:
+        break
