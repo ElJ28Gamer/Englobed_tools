99d11f26378252a9c603ad0cffd33874835fe15e
ElJ28Gamer <elj28fer@gmail.com>
2025-10-26 19:52:31 +0100

    test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Imports
 import cv2 as cv                 #Camera lib
+import os
 
 #Variables
 numberlist = [1, 2, 3, 4]
@@ -10,9 +11,9 @@ def menu():                      #Prints what tools have
     print("//  Englobed Tools  //")
     print("")
     print(" - Press 1 to open the camera")
-    print(" - Press 2 to ?")
-    print(" - Press 3 to ?")
-    print(" - Press 4 to ?")
+    print(" - Press 2 to open a list of all the WIFI's you have connected to. (Only in Windows)")
+    print(" - Press 3 to ")
+    print(" - Press 4 to ")
     print(" - To exit, press Enter")
     print("")
     print("//  Englobed Tools  //")
@@ -23,7 +24,7 @@ def selection():
      cam()
      loop()
     elif x == 2:        #Opens tool 2
-     cam()
+     showifi()
      loop()
     elif x == 3:        #Opens tool 3
      cam()
@@ -46,14 +47,19 @@ def cam():
     if not ret:
         print("exit")
         break
-
+        
     color = cv.cvtColor(frame, cv.COLOR_BGR2BGRA)
-    cv.imshow("CUSTOM CAMERA", color)                               #Custom name
+    cv.imshow("Camera", color)                               #Custom name
     if cv.waitKey(1) == ord("q") or cv.waitKey(1) == ord("Q"):      #Keys to exit
         break
 
  cap.release()                  #Stops capture
  cv.destroyAllWindows()         #Closes window
+
+def showifi():
+    print("")
+    os.system("wifi.bat")
+    print("")
 
 def loop():
     menu()
